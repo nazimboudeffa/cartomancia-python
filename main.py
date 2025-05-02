f5652f9563dd1202b05a3c6e5080482a89d84552
Nazim Boudeffa <boudeffa@gmail.com>
2025-05-02 18:57:06 +0200

    added more interpretation

--- main.py
+++ main.py
@@ -2,15 +2,45 @@ import random
 
 # Dictionnaire des significations simples pour chaque carte
 card_meanings = {
-    ('A', 'H'): "Amour sincère",
-    ('K', 'H'): "Protection d’un homme",
-    ('Q', 'H'): "Femme bienveillante",
-    ('J', 'H'): "Nouvelles liées à l’amour",
+    # Cœurs (H - Hearts) : domaine affectif, famille, amour
+    ('7', 'H'): "Petit bonheur affectif",
+    ('8', 'H'): "Pensées amoureuses",
+    ('9', 'H'): "Bonheur tranquille",
     ('T', 'H'): "Joie inattendue",
-    ('9', 'H'): "Bonheur simple",
-    ('8', 'H'): "Pensées d’amour",
-    ('7', 'H'): "Petit plaisir affectif",
-    # Tu feras de même pour D, C, S…
+    ('J', 'H'): "Message sentimental",
+    ('Q', 'H'): "Femme sincère et aimante",
+    ('K', 'H'): "Homme protecteur et bienveillant",
+    ('A', 'H'): "Amour sincère et durable",
+
+    # Carreaux (D - Diamonds) : domaine matériel, argent, communication
+    ('7', 'D'): "Gain inattendu",
+    ('8', 'D'): "Invitation ou lettre",
+    ('9', 'D'): "Affaires prospères",
+    ('T', 'D'): "Déplacement favorable",
+    ('J', 'D'): "Nouvelle professionnelle",
+    ('Q', 'D'): "Femme d’affaires ou intéressée",
+    ('K', 'D'): "Homme influent, financier",
+    ('A', 'D'): "Réussite financière ou professionnelle",
+
+    # Trèfles (C - Clubs) : chance, travail, réussite
+    ('7', 'C'): "Petit gain ou aide",
+    ('8', 'C'): "Nouvelles liées au travail",
+    ('9', 'C'): "Succès dans un projet",
+    ('T', 'C'): "Chance ou opportunité",
+    ('J', 'C'): "Bonne surprise",
+    ('Q', 'C'): "Femme active et travailleuse",
+    ('K', 'C'): "Homme ambitieux et chanceux",
+    ('A', 'C'): "Réussite générale, chance",
+
+    # Piques (S - Spades) : domaine des épreuves, décisions, spiritualité
+    ('7', 'S'): "Retard ou obstacle mineur",
+    ('8', 'S'): "Préoccupation ou inquiétude",
+    ('9', 'S'): "Déception ou tristesse",
+    ('T', 'S'): "Changement inévitable",
+    ('J', 'S'): "Message conflictuel",
+    ('Q', 'S'): "Femme froide ou distante",
+    ('K', 'S'): "Homme autoritaire ou difficile",
+    ('A', 'S'): "Décision importante, parfois rupture",
 }
 
 def create_deck():
